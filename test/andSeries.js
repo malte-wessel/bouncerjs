728a32888738951385ac7ffdbb4a72d7e4e321bd
Malte W <wessel@rheinschafe.de>
2014-12-11 19:03:15 +0100

    test andSeries

---
+++ test/andSeries.js
@@ -0,0 +1,42 @@
+var andSeries = require('../src/utils').andSeries;
+var test = require('tape');
+
+function eachIterator(args, x, callback) {
+	setTimeout(function(){
+		args.push(x);
+		callback();
+	}, x * 25);
+}
+
+test('andSeries', function (t) {
+	var args = [];
+	andSeries([1, 3, 2], eachIterator.bind(this, args), function(err) {
+		t.deepEqual(args, [1, 3, 2]);
+		t.end();
+	});
+});
+
+test('andSeries empty array', function (t) {
+	andSeries([], function(x, callback){
+		t.ok(false, 'iterator should not be called');
+		callback();
+	}, function(err){
+		t.ok(err instanceof Error, 'should pass error');
+	});
+
+	setTimeout(t.end, 25);
+});
+
+test('break loop on error', function (t) {
+	var counter = 0;
+	var itr = function(x, callback) {
+		counter++;
+		callback(new Error());
+	};
+
+	andSeries([1, 2], itr, function(err) {
+		t.ok(counter === 1, 'should break loop');
+		t.ok(err instanceof Error, 'should pass error');
+		t.end();
+	});
+});
